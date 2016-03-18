7bf81c735dd68210867128f99802ad8c4084028b
Ryan Ludwig <rludwig@vernier.com>
2016-03-18 09:40:01 -0700

    Various tweaks to make Chrome packaged apps run smoothly

--- src/supports/supports-cache.js
+++ src/supports/supports-cache.js
@@ -12,39 +12,15 @@ function readLocalStorage(key) {
   // allow reading from storage to retrieve previous support results
   // even while the document does not have focus
   let data;
-
-  try {
-    data = window.localStorage && window.localStorage.getItem(key);
-    data = data ? JSON.parse(data) : {};
-  } catch (e) {
-    data = {};
-  }
-
+  data = {};
   return data;
 }
 
 function writeLocalStorage(key, value) {
-  if (!document.hasFocus()) {
-    // if the document does not have focus when tests are executed, focus() may
-    // not be handled properly and events may not be dispatched immediately.
-    // This can happen when a document is reloaded while Developer Tools have focus.
-    try {
-      window.localStorage && window.localStorage.removeItem(key);
-    } catch (e) {
-      // ignore
-    }
-
-    return;
-  }
-
-  try {
-    window.localStorage && window.localStorage.setItem(key, JSON.stringify(value));
-  } catch (e) {
-    // ignore
-  }
+  return
 }
 
-const userAgent = typeof window !== 'undefined' && window.navigator.userAgent || '';
+const userAgent = '';
 const cacheKey = 'ally-supports-cache';
 let cache = readLocalStorage(cacheKey);
 
